2000b906a2fd49d4808fbd5375394a93408786ef
donbrae <jamie@donbrae.co.uk>
2017-04-29 18:04:59 +0100

    Working widget

---
+++ discourse-widget.php
@@ -0,0 +1,92 @@
+<div class="post-1741 post type-post format-aside hentry post_format-post-format-aside">
+    <table class="discourse-widget ds-widget">
+        <thead>
+            <tr>
+                <th>Economic growth. Source: <a href="http://www.gov.scot/Topics/Statistics/Browse/Economy/PubGDP/GDP2016Q4/DOCS">gov.scot</a></th>
+                <th>GDP per capita</th>
+                <th>Unemployment</th>
+                <th colspan="2">Currency (£). Source: <a href="http://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html">ECB</a>; usually updated at 16:00 CET (working days)</th>
+                <th>NYMEX Brent Crude Oil</th>
+            </tr>
+            <tr>
+                <th>Q-on-Q growth rate</th>
+                <th></th>
+                <th>Dec 16 – Feb 17</th>
+                <th colspan="2"><span class="ds-forex ds-forex-date ds-fade"></span></th>
+                <th><span class="ds-brent ds-brent-date ds-fade"></span></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <ul>
+                        <li>0.0 (2016 Q1)</li>
+                        <li><span class="highlight-green">0.1</span> (2016 Q2)</li>
+                        <li><span class="highlight-green">0.1</span> (2016 Q3)</li>
+                        <li><span class="highlight-red">-0.2</span> (2016 Q4)</li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><a href="http://www.gov.scot/Resource/0051/00514410.pdf">£29,300</a> (Oct 2015 – Sep 2016)</li>
+                        <li><a href="http://www.gov.scot/Resource/0049/00499946.pdf">£28,400</a> (2015)</li>
+                    </ul>
+                </td>
+                <td><a href="https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/datasets/summaryoflabourmarketstatistics">4.5%</a> (<span class="highlight-green">-1.6%</span> year on year)</td>
+                <td><span class="ds-forex ds-fade">£1 = €<span class="ds-euro-val"></span> (<span class="ds-euro-chg"></span>)</span></td>
+                <td><span class="ds-forex ds-fade">£1 = $<span class="ds-usd-val"></span> (<span class="ds-usd-chg"></span>)</span></td>
+                <td><span class="ds-brent ds-fade"><a href="http://www.quandl.com/CHRIS/CME_BZ1"><span class="ds-brent-val"></span></a> dollars per barrel (<span class="ds-brent-chg"></span>)</span></td>
+            </tr>
+        </tbody>
+    </table>
+    <table class="discourse-widget ds-widget-stacked">
+        <thead>
+            <tr>
+                <th>Economic growth. Source: <a href="http://www.gov.scot/Topics/Statistics/Browse/Economy/PubGDP/GDP2016Q4/DOCS">gov.scot</a></th>
+                <th>GDP per capita</th>
+                <th>Unemployment</th>
+            </tr>
+            <tr>
+                <th>Q-on-Q growth rate</th>
+                <th></th>
+                <th>Dec 16 – Feb 17</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>
+                    <ul>
+                        <li>0.0 (2016 Q1)</li>
+                        <li><span class="highlight-green">0.1</span> (2016 Q2)</li>
+                        <li><span class="highlight-green">0.1</span> (2016 Q3)</li>
+                        <li><span class="highlight-red">-0.2</span> (2016 Q4)</li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><a href="http://www.gov.scot/Resource/0051/00514410.pdf">£29,300</a> (Oct 2015 – Sep 2016)</li>
+                        <li><a href="http://www.gov.scot/Resource/0049/00499946.pdf">£28,400</a> (2015)</li>
+                    </ul>
+                </td>
+                <td><a href="https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/datasets/summaryoflabourmarketstatistics">4.5%</a> (<span class="highlight-green">-1.6%</span> year on year)</td>
+            </tr>
+        </tbody>
+        <thead>
+            <tr>
+                <th colspan="2">Currency (£). Source: <a href="http://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html">ECB</a>; usually updated at 16:00 CET (working days)</th>
+                <th>NYMEX Brent Crude Oil</th>
+            </tr>
+            <tr>
+                <th colspan="2"><span class="ds-forex ds-forex-date ds-fade"><span aria-hidden="true">Placeholder</span></span></th>
+                <th><span class="ds-brent ds-brent-date ds-fade"></span></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><span class="ds-forex ds-fade">£1 = €<span class="ds-euro-val"></span> (<span class="ds-euro-chg"></span>)</span></td>
+                <td><span class="ds-forex ds-fade">£1 = $<span class="ds-usd-val"></span> (<span class="ds-usd-chg"></span>)</span></td>
+                <td><span class="ds-brent ds-fade"><a href="http://www.quandl.com/CHRIS/CME_BZ1"><span class="ds-brent-val"></span></a> dollars per barrel (<span class="ds-brent-chg"></span>)</span></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
--- discourse.js
+++ discourse.js
@@ -2,123 +2,153 @@
  * Fetches forex and Brent crude oil prices for Discourse.scot widget
  */
 
-$(function() {
-    var cfg = {
-            base: 'GBP',
-            symbols: 'USD,EUR'
-        },
-        brent_crude = {},
-        forex = {},
-        date = getDate(),
-        date_previous;
+(function($) {
+    $(function() {
+        var cfg = {
+                base: 'GBP',
+                symbols: 'USD,EUR'
+            },
+            brent_crude = {},
+            forex = {},
+            date = getDate(),
+            date_previous;
 
-    /**
-     * Gets date in YYYY-MM-DD format
-     * @param {String} date (optional) - e.g. '2017-04-27'
-     * @returns {String}
-     */
-    function getDate(date) { // From http://stackoverflow.com/a/4929629
-        var d = date ? new Date(date) : new Date(),
-            dd = d.getDate(),
-            mm = d.getMonth() + 1, // January is 0!
-            yyyy = d.getFullYear();
+        /**
+         * Gets date in YYYY-MM-DD format
+         * @param {String} date (optional) - e.g. '2017-04-27'
+         * @returns {String}
+         */
+        function getDate(date) { // From http://stackoverflow.com/a/4929629
+            var d = date ? new Date(date) : new Date(),
+                dd = d.getDate(),
+                mm = d.getMonth() + 1, // January is 0!
+                yyyy = d.getFullYear();
 
-        if (dd < 10) {
-            dd = '0' + dd;
+            if (dd < 10) {
+                dd = '0' + dd;
+            }
+
+            if (mm < 10) {
+                mm = '0' + mm;
+            }
+
+            return yyyy + '-' + mm + '-' + dd;
         }
 
-        if (mm < 10) {
-            mm = '0' + mm;
+        /**
+         * Formats the change percentage: adds + or - and relevant colour class
+         * @param {Number} val
+         * @param {Strings} selector
+         */
+        function formatPCChange(val, selector) {
+
+            if (val > 0) {
+                $(selector).text('+' + val + '%').addClass('highlight-green');
+            } else if (val < 0) {
+                $(selector).text(val + '%').addClass('highlight-red');
+            } else if (val === 0) {
+                $(selector).text('No change');
+            }
         }
 
-        return yyyy + '-' + mm + '-' + dd;
-    }
+        if ($('.discourse-widget').length) {
+            /**
+             * Forex
+             * Alternative 1: https://www.quandl.com/api/v3/datasets/ECB/EURGBP?limit=2
+             * Alternative 2a (not as timely as fixer.io): https://www.quandl.com/api/v3/datasets/BOE/XUDLGBD?limit=2 (rate for GBP to USD)
+             * Alternative 2b (not as timely as fixer.io): https://www.quandl.com/api/v3/datasets/BOE/XUDLSER?limit=2 (rate for GBP to EUR)
+             */
 
-    /**
-     * Forex
-     * Alternative 1: https://www.quandl.com/api/v3/datasets/ECB/EURGBP?limit=2
-     * Alternative 2a (not as timely as fixer.io): https://www.quandl.com/api/v3/datasets/BOE/XUDLGBD?limit=2 (rate for GBP to USD)
-     * Alternative 2b (not as timely as fixer.io): https://www.quandl.com/api/v3/datasets/BOE/XUDLSER?limit=2 (rate for GBP to EUR)
-     */
-
-    // Most recent
-    $.ajax({
-        type: 'GET',
-        url: 'http://api.fixer.io/latest' + date,
-        data: {
-            base: cfg.base,
-            symbols: cfg.symbols
-        },
-        dataType: 'json',
-        success: function(d) {
-            forex.latest = d;
-            date_previous = new Date(forex.latest.date);
-            date_previous.setDate(date_previous.getDate() - 1);
-            date_previous = getDate(date_previous);
-
-            // Previous day
+            // Most recent
             $.ajax({
                 type: 'GET',
-                url: 'http://api.fixer.io/' + date_previous,
+                url: 'https://api.fixer.io/latest' + date,
                 data: {
                     base: cfg.base,
                     symbols: cfg.symbols
                 },
                 dataType: 'json',
                 success: function(d) {
-                    forex.previous = d;
-                    forex.one_day_pc_change = {};
+                    forex.latest = d;
+                    date_previous = new Date(forex.latest.date);
+                    date_previous.setDate(date_previous.getDate() - 1);
+                    date_previous = getDate(date_previous);
 
-                    $.each(forex.latest.rates, function(k, v) {
-                        // 1 day % change
-                        forex.one_day_pc_change[k] = (((forex.latest.rates[k] - forex.previous.rates[k]) / forex.previous.rates[k]) * 100).toFixed(2);
+                    // Previous day
+                    $.ajax({
+                        type: 'GET',
+                        url: 'https://api.fixer.io/' + date_previous,
+                        data: {
+                            base: cfg.base,
+                            symbols: cfg.symbols
+                        },
+                        dataType: 'json',
+                        success: function(d) {
+                            forex.previous = d;
+                            forex.one_day_pc_change = {};
+
+                            $.each(forex.latest.rates, function(k, v) {
+                                // 1 day % change
+                                forex.one_day_pc_change[k] = (((forex.latest.rates[k] - forex.previous.rates[k]) / forex.previous.rates[k]) * 100).toFixed(2);
+                            });
+
+                            $('.ds-forex-date').text(forex.latest.date + ' (change from ' + forex.previous.date + ')');
+                            $('.ds-euro-val').text(forex.latest.rates.EUR);
+                            $('.ds-usd-val').text(forex.latest.rates.USD);
+                            formatPCChange(forex.one_day_pc_change.EUR, '.ds-euro-chg');
+                            formatPCChange(forex.one_day_pc_change.USD, '.ds-usd-chg');
+                            $('.ds-forex').addClass('ds-in');
+
+                            console.log(forex);
+                        },
+                        error: function(type, xhr) {
+                            console.log(type + ': ' + xhr.responseText);
+                        }
                     });
-
-                    console.log(forex);
                 },
                 error: function(type, xhr) {
                     console.log(type + ': ' + xhr.responseText);
                 }
             });
-        },
-        error: function(type, xhr) {
-            console.log(type + ': ' + xhr.responseText);
+
+            /**
+             * Brent crude
+             */
+            $.ajax({
+                type: 'GET',
+                url: 'https://www.quandl.com/api/v1/datasets/CHRIS/CME_BZ1.json',
+                data: {
+                    limit: '2',
+                    api_key: '5Vms_cFiREVBAeztLzE1'
+                },
+                dataType: 'json',
+                success: function(d) {
+                    var o;
+
+                    // Latest price
+                    o = {};
+                    o.date = d.data[0][0];
+                    o.price_settle = d.data[0][6].toFixed(2);
+                    brent_crude.latest = o;
+
+                    // Previous price
+                    o = {};
+                    o.date = d.data[1][0];
+                    o.price_settle = d.data[1][6].toFixed(2);
+                    brent_crude.previous = o;
+
+                    // 1 day % change
+                    brent_crude.one_day_pc_change = (((brent_crude.latest.price_settle - brent_crude.previous.price_settle) / brent_crude.previous.price_settle) * 100).toFixed(2);
+
+                    $('.ds-brent-date').text(brent_crude.latest.date + ' (change from ' + brent_crude.previous.date + ')');
+                    $('.ds-brent-val').text(brent_crude.latest.price_settle);
+                    formatPCChange(brent_crude.one_day_pc_change, '.ds-brent-chg');
+                    $('.ds-brent').addClass('ds-in');
+                },
+                error: function(type, xhr) {
+                    console.log(type + ': ' + xhr.responseText);
+                }
+            });
         }
     });
-
-    /**
-     * Brent crude
-     */
-    $.ajax({
-        type: 'GET',
-        url: 'https://www.quandl.com/api/v1/datasets/CHRIS/CME_BZ1.json',
-        data: {
-            limit: '2',
-            api_key: '5Vms_cFiREVBAeztLzE1'
-        },
-        dataType: 'json',
-        success: function(d) {
-            var o;
-
-            // Latest price
-            o = {};
-            o.date = d.data[0][0];
-            o.price_settle = d.data[0][6].toFixed(2);
-            brent_crude.latest = o;
-
-            // Previous price
-            o = {};
-            o.date = d.data[1][0];
-            o.price_settle = d.data[1][6].toFixed(2);
-            brent_crude.previous = o;
-
-            // 1 day % change
-            brent_crude.one_day_pc_change = (((brent_crude.latest.price_settle - brent_crude.previous.price_settle) / brent_crude.previous.price_settle) * 100).toFixed(2);
-
-            console.log(brent_crude);
-        },
-        error: function(type, xhr) {
-            console.log(type + ': ' + xhr.responseText);
-        }
-    });
-});
+})(jQuery);
