dffbc84b90252a85ea26f2a0bac1d4ebad81c60a
donbrae <jamie@donbrae.co.uk>
2017-07-13 19:44:49 +0100

    Updated with latest data

--- discourse-widget.php
+++ discourse-widget.php
@@ -2,16 +2,16 @@
     <table class="discourse-widget ds-widget">
         <thead>
             <tr>
-                <th>Economic growth. Source: <a href="http://www.gov.scot/Topics/Statistics/Browse/Economy/PubGDP/GDP2016Q4/DOCS">gov.scot</a></th>
-                <th>GDP per capita. Source: <a href="http://www.gov.scot/Resource/0051/00517685.pdf">gov.scot</a></th>
+                <th>GDP growth. Source:&nbsp;<a href="http://www.gov.scot/Topics/Statistics/Browse/Economy/PubGDP/GDP2017Q1/DOCS">gov.scot</a></th>
+                <th>GDP growth by industry. Source:&nbsp;<a href="http://www.gov.scot/Topics/Statistics/Browse/Economy/PubGDP/GDP2017Q1/DOCS">gov.scot</a></th>
                 <th>Unemployment</th>
-                <th colspan="2">Currency (£). Source: <a href="http://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html">ECB</a>; usually updated at 16:00 CET (working days)</th>
+                <th colspan="2">Currency&nbsp;(£). Source: <a href="http://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html">ECB</a>; usually updated at 16:00 CET&nbsp;(working days)</th>
                 <th>NYMEX Brent Crude Oil</th>
             </tr>
             <tr class="sub">
-                <th>Q-on-Q growth rate</th>
-                <th>Includes geographical share of extra-regio</th>
-                <th>Jan – Mar 2017</th>
+                <th>Q-on-Q growth</th>
+                <th>2017 Q1 on 2016 Q4</th>
+                <th>Mar - May 2017</th>
                 <th colspan="2"><span class="ds-forex ds-forex-date ds-fade"></span></th>
                 <th><span class="ds-brent ds-brent-date ds-fade"></span></th>
             </tr>
@@ -20,25 +20,27 @@
             <tr>
                 <td>
                     <ul>
-                        <li>0.0 (2016 Q1)</li>
-                        <li><span class="highlight-green">0.1</span> (2016 Q2)</li>
-                        <li><span class="highlight-green">0.1</span> (2016 Q3)</li>
-                        <li><span class="highlight-red">-0.2</span> (2016 Q4)</li>
+                        <li><span class="highlight-green">+0.1%</span> (2016 Q2)</li>
+                        <li><span class="highlight-green">+0.1%</span> (2016 Q3)</li>
+                        <li><span class="highlight-red">-0.2%</span> (2016 Q4)</li>
+                        <li><span class="highlight-green">+0.8%</span> (2017 Q1)</li>
                     </ul>
                 </td>
                 <td>
                     <ul>
-                            <li>£29,554 (2016)</li>
-                            <li>£29,107 (2015)</li>
-                            <li>£29,838 (2014)</li>
-                            <li>£29,391 (2013)</li>
-                            <li>£28,357 (2012)</li>
+                        <li>Agriculture, Forestry &amp; Fishing: <span class="highlight-green">+0.4%</span></li>
+                        <li>Production: <span class="highlight-green">+3.1%</span></li>
+                        <li>Construction: <span class="highlight-red">-0.7%</span></li>
+                        <li>Services: <span class="highlight-green">+0.3%</span></li>
                     </ul>
                 </td>
-                <td><a href="https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/datasets/summaryoflabourmarketstatistics">4%</a> (<span class="highlight-green">-1.8%</span> year on year)</td>
-                <td><span class="ds-forex ds-fade">£1 = €<span class="ds-euro-val"></span> (<span class="ds-euro-chg"></span>)</span></td>
-                <td><span class="ds-forex ds-fade">£1 = $<span class="ds-usd-val"></span> (<span class="ds-usd-chg"></span>)</span></td>
-                <td><span class="ds-brent ds-fade"><a href="http://www.quandl.com/CHRIS/CME_BZ1"><span class="ds-brent-val"></span></a> dollars per barrel (<span class="ds-brent-chg"></span>)</span></td>
+                <td><a href="https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/datasets/summaryoflabourmarketstatistics">3.8%</a> (<span class="highlight-green">-1.6%</span>&nbsp;year on year)</td>
+                <td><span class="ds-forex ds-fade">£1 = €<span class="ds-euro-val"></span> (<span class="ds-euro-chg"></span>)</span>
+                </td>
+                <td><span class="ds-forex ds-fade">£1 = $<span class="ds-usd-val"></span> (<span class="ds-usd-chg"></span>)</span>
+                </td>
+                <td><span class="ds-brent ds-fade"><a href="http://www.quandl.com/CHRIS/CME_BZ1"><span class="ds-brent-val"></span></a> dollars per barrel (<span class="ds-brent-chg"></span>)</span>
+                </td>
             </tr>
         </tbody>
     </table>
@@ -46,54 +48,183 @@
     <table class="discourse-widget ds-widget-stacked">
         <thead>
             <tr>
-                <th>Economic growth. Source: <a href="http://www.gov.scot/Topics/Statistics/Browse/Economy/PubGDP/GDP2016Q4/DOCS">gov.scot</a></th>
-                <th>GDP per capita</th>
+                <th>GDP growth. Source:&nbsp;<a href="http://www.gov.scot/Topics/Statistics/Browse/Economy/PubGDP/GDP2017Q1/DOCS">gov.scot</a></th>
+                <th>GDP growth by industry. Source:&nbsp;<a href="http://www.gov.scot/Topics/Statistics/Browse/Economy/PubGDP/GDP2017Q1/DOCS">gov.scot</a></th>
                 <th>Unemployment</th>
             </tr>
             <tr class="sub">
-                <th>Q-on-Q growth rate</th>
-                <th></th>
-                <th>Jan – Mar 2017</th>
+                <th>Q-on-Q growth</th>
+                <th>2017 Q1 on 2016 Q4</th>
+                <th>Mar - May 2017</th>
             </tr>
         </thead>
         <tbody>
             <tr>
                 <td>
                     <ul>
-                        <li>0.0 (2016 Q1)</li>
-                        <li><span class="highlight-green">0.1</span> (2016 Q2)</li>
-                        <li><span class="highlight-green">0.1</span> (2016 Q3)</li>
-                        <li><span class="highlight-red">-0.2</span> (2016 Q4)</li>
+                        <li><span class="highlight-green">+0.1%</span> (2016 Q2)</li>
+                        <li><span class="highlight-green">+0.1%</span> (2016 Q3)</li>
+                        <li><span class="highlight-red">-0.2%</span> (2016 Q4)</li>
+                        <li><span class="highlight-green">+0.8%</span> (2017 Q1)</li>
                     </ul>
                 </td>
                 <td>
                     <ul>
-                        <li>£29,554 (2016)</li>
-                        <li>£29,107 (2015)</li>
-                        <li>£29,838 (2014)</li>
-                        <li>£29,391 (2013)</li>
-                        <li>£28,357 (2012)</li>
+                        <li>Agriculture, Forestry &amp; Fishing: <span class="highlight-green">+0.4%</span></li>
+                        <li>Production: <span class="highlight-green">+3.1%</span></li>
+                        <li>Construction: <span class="highlight-red">-0.7%</span></li>
+                        <li>Services: <span class="highlight-green">+0.3%</span></li>
                     </ul>
                 </td>
-                <td><a href="https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/datasets/summaryoflabourmarketstatistics">4%</a> (<span class="highlight-green">-1.8%</span> year on year)</td>
+                <td><a href="https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/employmentandemployeetypes/datasets/summaryoflabourmarketstatistics">3.8%</a> (<span class="highlight-green">-1.6%</span>&nbsp;year on year)</td>
             </tr>
         </tbody>
         <thead>
             <tr>
-                <th colspan="2">Currency (£). Source: <a href="http://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html">ECB</a>; usually updated at 16:00 CET (working days)</th>
+                <th colspan="2">Currency&nbsp;(£). Source: <a href="http://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html">ECB</a>; usually updated at 16:00 CET&nbsp;(working days)</th>
                 <th>NYMEX Brent Crude Oil</th>
             </tr>
             <tr class="sub">
-                <th colspan="2"><span class="ds-forex ds-forex-date ds-fade"><span aria-hidden="true">Placeholder</span></span></th>
+                <th colspan="2"><span class="ds-forex ds-forex-date ds-fade"><span aria-hidden="true">Placeholder</span></span>
+                </th>
                 <th><span class="ds-brent ds-brent-date ds-fade"></span></th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td><span class="ds-forex ds-fade">£1 = €<span class="ds-euro-val"></span> (<span class="ds-euro-chg"></span>)</span></td>
-                <td><span class="ds-forex ds-fade">£1 = $<span class="ds-usd-val"></span> (<span class="ds-usd-chg"></span>)</span></td>
-                <td><span class="ds-brent ds-fade"><a href="http://www.quandl.com/CHRIS/CME_BZ1"><span class="ds-brent-val"></span></a> dollars per barrel (<span class="ds-brent-chg"></span>)</span></td>
+                <td><span class="ds-forex ds-fade">£1 = €<span class="ds-euro-val"></span> (<span class="ds-euro-chg"></span>)</span>
+                </td>
+                <td><span class="ds-forex ds-fade">£1 = $<span class="ds-usd-val"></span> (<span class="ds-usd-chg"></span>)</span>
+                </td>
+                <td><span class="ds-brent ds-fade"><a href="http://www.quandl.com/CHRIS/CME_BZ1"><span class="ds-brent-val"></span></a> dollars per barrel (<span class="ds-brent-chg"></span>)</span>
+                </td>
             </tr>
         </tbody>
     </table>
+    <!--more-->
+    <div id="highcharts-asepiry">
+        <script src="//cloud.highcharts.com/inject/asepiry" defer="defer"></script>
+    </div>
+    <hr>
+    <div id="highcharts-uduliwe">
+        <script src="//cloud.highcharts.com/inject/uduliwe" defer="defer"></script>
+    </div>
+    <hr>
+    <div id="highcharts-akypycu">
+        <script src="//cloud.highcharts.com/inject/akypycu" defer="defer"></script>
+    </div>
+    <hr>
+    <div id="highcharts-opyzuda">
+        <script src="//cloud.highcharts.com/inject/opyzuda" defer="defer"></script>
+    </div>
+    <hr>
+    <div id="highcharts-ajekexy">
+        <script src="//cloud.highcharts.com/inject/ajekexy" defer="defer"></script>
+    </div>
+    <hr>
+    <div id="highcharts-eqivuqi">
+        <script src="//cloud.highcharts.com/inject/eqivuqi" defer="defer"></script>
+    </div>
+    <hr>
+    <h4>Number of Foreign Direct Investment (FDI) projects secured, 2016</h4>
+    <table>
+        <thead>
+            <tr>
+                <th>Country/region</th>
+                <th>FDI projects secured in 2015</th>
+                <th>FDI projects secured in 2016</th>
+                <th>Change: 2015 to 2016</th>
+                <th>2016 Ranking</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>London</td>
+                <td>405</td>
+                <td>451</td>
+                <td>+46</td>
+                <td>1</td>
+            </tr>
+            <tr class="bg-highlight">
+                <td>Scotland</td>
+                <td>119</td>
+                <td>122</td>
+                <td>+3</td>
+                <td>2</td>
+            </tr>
+            <tr>
+                <td>West Midlands</td>
+                <td>92</td>
+                <td>111</td>
+                <td>+19</td>
+                <td>3</td>
+            </tr>
+            <tr>
+                <td>Yorkshire</td>
+                <td>83</td>
+                <td>98</td>
+                <td>+15</td>
+                <td>4</td>
+            </tr>
+            <tr>
+                <td>North West England</td>
+                <td>101</td>
+                <td>90</td>
+                <td>-11</td>
+                <td>5</td>
+            </tr>
+            <tr>
+                <td>South East England</td>
+                <td>61</td>
+                <td>72</td>
+                <td>+11</td>
+                <td>6</td>
+            </tr>
+            <tr>
+                <td>East Midlands</td>
+                <td>37</td>
+                <td>44</td>
+                <td>+7</td>
+                <td>7</td>
+            </tr>
+            <tr>
+                <td>East of England</td>
+                <td>28</td>
+                <td>39</td>
+                <td>+11</td>
+                <td>8</td>
+            </tr>
+            <tr>
+                <td>Northern Ireland</td>
+                <td>15</td>
+                <td>39</td>
+                <td>+24</td>
+                <td>9</td>
+            </tr>
+            <tr>
+                <td>South West England</td>
+                <td>37</td>
+                <td>28</td>
+                <td>-9</td>
+                <td>10</td>
+            </tr>
+            <tr>
+                <td>North East England</td>
+                <td>41</td>
+                <td>26</td>
+                <td>-15</td>
+                <td>11</td>
+            </tr>
+            <tr>
+                <td>Wales</td>
+                <td>41</td>
+                <td>23</td>
+                <td>-18</td>
+                <td>12</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <p><small>Source: EY’s <a href="http://www.ey.com/Publication/vwLUAssets/EY-attractiveness-survey-Scotland-2017/$FILE/EY-Scotland-Attractiveness-Survey-2017.pdf">2017 Scotland Attractiveness Survey</a>.</small></p>
+
 </div>
